a3069035b16a6a717420c248f7ed08e4efb9479a
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-09-07 00:06:52 +0300

    Add files via upload

--- hutoryanin/settings.py
+++ hutoryanin/settings.py
@@ -81,17 +81,13 @@ WSGI_APPLICATION = 'hutoryanin.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     },
-}
-
-'''
-DATABASES = {
-    'default': {
+    'postgresql': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'dcfjk6lm9acdo2',
         'USER': 'mkkkvlbuncdczv',
