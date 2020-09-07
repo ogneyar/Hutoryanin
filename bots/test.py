6e776aac6df9c8d0d5d880abf7986f54d1db0aa2
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-09-07 17:06:51 +0300

    Add files via upload

--- bots/test.py
+++ bots/test.py
@@ -1,47 +1,69 @@
-﻿{% extends "base.html" %}
-{% load static %}
+﻿from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 
-{% block header %}
+import requests
+import json
+import os
+from bs4 import BeautifulSoup
 
-<div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-
-  <h1 class="display-4">Техническая поддержка</h1>
-  
-  <p class="lead">Здесь Вы можете к нам обратиться с вопросами и предложениями.</p>
-  
-</div>
-
-{% endblock %}
-
-{% block content %}
-
-<div class="card-deck mb-3 text-center">
-  
-    <div class="card mb-4 shadow-sm">
-      <div class="card-header">
-        <h4 class="my-0 font-weight-normal">Напишите</h4>
-      </div>
-      <div class="card-body">
-        <form method="post">
-			<input type="hidden" name="login" id="login" value="test">
-			
-			<label>Ваш Email:</label><br>
-			<input type="text" placeholder="Email" name="email" id="email" maxlength="100"><br><br>
-			
-			<label>Текст сообщения:</label><br>
-			<div contenteditable="true">
-				<textarea placeholder="Здесь напишите Ваш вопрос или предложение" name="message" id="message" maxlength="1500" wrap="soft"></textarea>
-			</div>
-			<br>
-			
-			<button type="button" class="btn btn-lg btn-block btn-outline-primary">Отправить</button>
-		</form>
-      </div>
-    </div>
-		
-</div>
-
-  
-{% endblock %}
+from classes.tg.botApi import Bot
+from classes.tg.types.replyKeyboardMarkup import ReplyKeyboardMarkup
+from classes.tg.types.chatPermissions import ChatPermissions
 
 
+def start(request):
+
+    headers = {
+        'User-Agent': "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36 OPR/70.0.3728.106",
+        'x-youtube-client-name': '1',
+        'x-youtube-client-version': '2.20200529.02.01'
+    }
+
+    #response = requests.get("https://www.youtube.com/watch?v=dGRJU_QlMf4&feature=youtu.be", headers=headers)
+
+    url = "https://www.youtube.com/watch?v=dGRJU_QlMf4&feature=youtu.be"
+
+    page = requests.get(url, headers=headers, timeout=(1, 3))
+
+    response = tg.sendMessage(chat_id, "Статус код: \n\n" + str(page.status_code))
+
+    soup = BeautifulSoup(page.text, "html.parser")
+
+    return HttpResponse(soup.prettify())
+
+
+    response = tg.sendMessage(chat_id, soup.title)
+
+    div = soup.find(id="guide-button")
+
+    response = tg.sendMessage(chat_id, repr(div))
+
+    #return HttpResponse('<br><br><br><br><br><center>O da!</center>')
+
+
+
+    '''
+    h1_class_title = soup.findAll('h1', class_='title style-scope ytd-video-primary-info-renderer')
+
+    response = tg.sendMessage(chat_id, repr(h1_class_title))
+    '''
+
+    #response = tg.sendMessage(chat_id, soup.body.find(id="description").span)
+
+    #response = tg.sendMessage(chat_id, str(soup.find(id="description")))
+
+    '''
+    new_news = []
+    news = []
+
+    soup = BeautifulSoup(page.text, "html.parser")
+
+    news = soup.findAll('div', class_='style-scope ytd-video-secondary-info-renderer')
+
+    for i in range(len(news)):
+        if news[i].find('span', class_='style-scope yt-formatted-string') is not None:
+            new_news.append(news[i].text)
+
+    for i in range(len(new_news)):
+        response = tg.sendMessage(chat_id, new_news[i])
+    '''
