fa48086f24426997a704be9e88e8fe0a44f6adf1
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-09-08 19:29:39 +0300

    Add files via upload

--- bots/test.py
+++ bots/test.py
@@ -10,7 +10,7 @@ import os
 from bs4 import BeautifulSoup
 
 # робота с куки и сессиями
-from django.shortcuts import redirect
+#from django.shortcuts import redirect
 
 
 import http.cookies
@@ -18,54 +18,55 @@ import http.cookies
 
 def start(request):
 
-    url = "http://127.0.0.1:8000/test/cookies/"
+    ''' передача куки
+    '''
 
-    r = requests.get(url)
+    url = "http://127.0.0.1:8000/test/cookie/"
+    cookies = {'cooka':'real'}
+    r = requests.get(url, cookies=cookies)
+    return HttpResponse( str(r.text) )
 
-
-    #if "cookie" in r.cookies:
-    if r.cookies.get('cook') is not None:
-
-        return HttpResponse( r.cookies.get('cook') )
-
-    else:
-        '''
-        cookies = {'cook':'real'}
-
-        r = requests.get(url, cookies=cookies)
-
-        return HttpResponse( "Установил cookie." + str(r.cookies.get('cook')) )
-        '''
+    # а приём с помощью request.COOKIES
 
 
-        ''' установка куки
-
-        jar = requests.cookies.RequestsCookieJar()
-        jar.set('first_cookie', 'first', domain='httpbin.org', path='/cookies')
-        jar.set('second_cookie', 'second', domain='httpbin.org', path='/extra')
-        jar.set('third_cookie', 'third', domain='httpbin.org', path='/cookies')
-
-        url = 'http://httpbin.org/cookies'
-        req = requests.get(url, cookies=jar)
-
-        return HttpResponse( "Установил cookie." + str(req.text) )
-
-        # returns '{ "cookies": { "first_cookie": "first", "third_cookie": "third" }}'
-
-        '''
 
 
-        ssn = requests.Session()
-        ssn.cookies.update({'visit-month': 'February'})
+    ''' передача куки c помощью сессии
 
-        #url = 'http://httpbin.org/cookies'
-        reqOne = ssn.get(url)
-        return HttpResponse( "Установил cookie." + str(reqOne.text) )
+    url = "http://127.0.0.1:8000/test/cookie/"
+    ssn = requests.Session()
+    ssn.cookies.update({'cooka2':'too good real'})
+    req = ssn.get(url)
+    return HttpResponse( str(req.text) )
+
+    '''
+
+
+
+    ''' НЕ РАБОТАЕТ установка куки при помощи RequestsCookieJar
+
+    jar = requests.cookies.RequestsCookieJar()
+    jar.set('first_cookie', 'first', domain='127.0.0.1:8000', path='/cookies')
+    jar.set('second_cookie', 'second', domain='127.0.0.1:8000', path='/extra')
+    jar.set('third_cookie', 'third', domain='127.0.0.1:8000', path='/cookies')
+
+    url = "http://127.0.0.1:8000/test/cookie/"
+    req = requests.get(url, cookies=jar)
+
+    return HttpResponse( str(req.text) )
+    # returns '{ "cookies": { "first_cookie": "first", "third_cookie": "third" }}'
+
+    '''
 
 
 def cookie(request):
 
-    return HttpResponse( "Куки" )
+    #r = HttpResponse()
+    #r.set_cookie("cooka", "real")
+
+
+    return HttpResponse( json.dumps(request.COOKIES) )
+
 
 
 def session(request):
@@ -88,19 +89,10 @@ def session(request):
 
 
 
-    ''' понял как передавать GET параметры
-
-    query = {'q': 'Forest', 'order': 'popular', 'min_width': '800', 'min_height': '600'}
-    req = requests.get('https://pixabay.com/en/photos/', params=query)
-
-    req.url
-    # returns 'https://pixabay.com/en/photos/?order=popular&min_height=600&q=Forest&min_width=800'
-
-    '''
-
-
+def parser(request):
 
     ''' работающая версия парсинга сайта
+    '''
 
     headers = {
         'User-Agent': "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36 OPR/70.0.3728.106",
@@ -119,7 +111,18 @@ def session(request):
 
     return HttpResponse( str(page.status_code)+"<br><br>"+str(soup.body.get_text()) )
 
-    '''
 
+
+
+
+    ''' понял как передавать GET параметры
+
+    query = {'q': 'Forest', 'order': 'popular', 'min_width': '800', 'min_height': '600'}
+    req = requests.get('https://pixabay.com/en/photos/', params=query)
+
+    req.url
+    # returns 'https://pixabay.com/en/photos/?order=popular&min_height=600&q=Forest&min_width=800'
+
+    '''
 
 
