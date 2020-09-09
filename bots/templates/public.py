fee6688b2bd8dc709499307848420af1d9c9fc21
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-09-09 15:20:57 +0300

    Add files via upload

--- bots/templates/public.py
+++ bots/templates/public.py
@@ -1,58 +1,63 @@
-from django.http import HttpResponse
-from django.http import HttpResponseRedirect
-from django.shortcuts import redirect
-
-import requests
-import json
-import os
-from bs4 import BeautifulSoup
-import bmemcached
+import os, bmemcached
 
 from classes.tg.botApi import Bot
-from classes.tg.types.replyKeyboardMarkup import ReplyKeyboardMarkup
-from classes.tg.types.chatPermissions import ChatPermissions
+
+class Public:
+
+    mc_servers = os.environ.get('MEMCACHIER_SERVERS', '').split(',')
+    mc_user = os.environ.get('MEMCACHIER_USERNAME', '')
+    mc_passw = os.environ.get('MEMCACHIER_PASSWORD', '')
+    token = os.getenv("TOKEN")
+    master = int(os.getenv("MASTER"))
+    debug = os.getenv("DEBUG")
+
+    mc = bmemcached.Client(mc_servers, username=mc_user, password=mc_passw)
+    mc.enable_retry_delay(True)
+
+    groupHutor = -464572634 # group
+    groupHutor2 = -1001471520704 # supergroup
+    groupHutor3 = -1001393395949 # supergroup
+
+    testerBotoff = 351009636
+
+    # инициализация телеграм бота
+    tg = Bot(token)
 
 
-mc_servers = os.environ.get('MEMCACHIER_SERVERS', '').split(',')
-mc_user = os.environ.get('MEMCACHIER_USERNAME', '')
-mc_passw = os.environ.get('MEMCACHIER_PASSWORD', '')
+    def __init__(self, request):
+        try:
 
-mc = bmemcached.Client(mc_servers, username=mc_user, password=mc_passw)
-mc.enable_retry_delay(True)
+            update = None
+            update = self.tg.start(request)
 
-token = os.getenv("TOKEN")
-master = int(os.getenv("MASTER"))
-debug = os.getenv("DEBUG")
+            message = update.getMessage()
+            if message is None:
+                editedMessage = update.getEditedMessage()
+                message = editedMessage
 
-groupHutor = -464572634 # group
-groupHutor2 = -1001471520704 # supergroup
-groupHutor3 = -1001393395949 # supergroup
+            if message is None:
+                channelPost = update.getChannelPost()
+                message = channelPost
 
-testerBotoff = 351009636
+            if message is None:
+                editedChannelPost = update.getEditedChannelPost()
+                message = editedChannelPost
 
-# инициализация телеграм бота
-tg = Bot(token)
-try:
+            message_id = message.getMessageId()
+            text = message.getText()
 
-    update = None
-    update = tg.start(request)
+            if (text == "/start"):
 
-    if (text == "/start"):
+                response = self.tg.sendMessage(master, "Зачем жмёшь старт?")
 
-        response = tg.sendMessage(chat_id, "Зачем жмёшь старт?")
+            else:
+                response = self.tg.sendMessage(master, "Ха!")
 
-    elif text == "":
+        except:
+
+            return HttpResponse("ok")
+
+
         return HttpResponse("ok")
 
-
-    else:
-        if (chat_type == "private"):
-            response = tg.sendMessage(chat_id, "Ха!")
-
-
-
-except:
-    return HttpResponse("ok")
-
-return HttpResponse("ok")
 
