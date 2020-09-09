4727b24d98d38d0f76d8fe0756a2afdcf5fa52f4
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-09-09 14:50:24 +0300

    Add files via upload

--- bots/templates/public.py
+++ bots/templates/public.py
@@ -20,103 +20,39 @@ mc_passw = os.environ.get('MEMCACHIER_PASSWORD', '')
 mc = bmemcached.Client(mc_servers, username=mc_user, password=mc_passw)
 mc.enable_retry_delay(True)
 
+token = os.getenv("TOKEN")
+master = int(os.getenv("MASTER"))
+debug = os.getenv("DEBUG")
 
-def bot(request):
-    try:
-        if (request.method == "POST"):
+groupHutor = -464572634 # group
+groupHutor2 = -1001471520704 # supergroup
+groupHutor3 = -1001393395949 # supergroup
 
-            token = os.getenv("TOKEN")
-            master = int(os.getenv("MASTER"))
-            debug = os.getenv("DEBUG")
+testerBotoff = 351009636
 
-            groupHutor = -464572634 # group
-            groupHutor2 = -1001471520704 # supergroup
-            groupHutor3 = -1001393395949 # supergroup
+# инициализация телеграм бота
+tg = Bot(token)
+try:
 
-            testerBotoff = 351009636
+    update = None
+    update = tg.start(request)
 
-            # инициализация телеграм бота
-            tg = Bot(token)
+    if (text == "/start"):
 
-            if (debug == "Да"):
-                response = tg.sendMessage(master, request.body, disable_web_page_preview=True)
-                #return HttpResponse("ok")
+        response = tg.sendMessage(chat_id, "Зачем жмёшь старт?")
 
-            update = None
-            update = tg.start(request)
-
-            if (debug == "Да"):
-                response = tg.sendMessage(master, update.getStr(), disable_web_page_preview=True)
-
-            message = update.getMessage()
-            if message is None:
-                editedMessage = update.getEditedMessage()
-                message = editedMessage
-
-            if message is None:
-                channelPost = update.getChannelPost()
-                message = channelPost
-
-            if message is None:
-                editedChannelPost = update.getEditedChannelPost()
-                message = editedChannelPost
-
-            if message is None:
-                poll = update.getPoll()
-                if poll is None:
-                    response = tg.sendMessage(master, "message,  channelPost and Poll is None")
-                else:
-                    response = tg.sendMessage(master, "This update is Poll")
-
-                return HttpResponse("ok")
-
-            new_chat_members = message.getNewChatMembers()
-            if new_chat_members != []:
-                new_chat_members_from_is_bot = new_chat_members[0].getIsBot()
-            else:
-                new_chat_members_from_is_bot = False
-            message_id = message.getMessageId()
-            text = message.getText()
-            #video_note = message.getVideoNote()
-
-            message_from = message.getFrom()
-            if message_from is not None:
-                from_id = message_from.getId()
-                from_first_name = message_from.getFirstName()
-                from_is_bot = message_from.getIsBot()
-            else:
-                from_id = 0
-                from_first_name = "unknown"
-                from_is_bot = False
-
-            chat = message.getChat()
-            chat_id = chat.getId()
-            chat_type = chat.getType()
-            chat_title = chat.getTitle()
-            if chat_title == "":
-                chat_title = "НЕИЗВЕСТНО"
-
-
-            if (text == "/start"):
-
-                response = tg.sendMessage(chat_id, "Зачем жмёшь старт?")
-
-
-
-
-
-            elif text == "":
-                return HttpResponse("ok")
-
-
-            else:
-                if (chat_type == "private"):
-                    response = tg.sendMessage(chat_id, "Ха!")
-
-
-
-    except Exception:
+    elif text == "":
         return HttpResponse("ok")
 
+
+    else:
+        if (chat_type == "private"):
+            response = tg.sendMessage(chat_id, "Ха!")
+
+
+
+except:
     return HttpResponse("ok")
+
+return HttpResponse("ok")
 
