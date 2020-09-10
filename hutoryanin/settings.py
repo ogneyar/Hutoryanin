8c3f829a28a442bd50ceee609322d28c9a13e11d
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-09-10 17:45:03 +0300

    Add files via upload

--- hutoryanin/settings.py
+++ hutoryanin/settings.py
@@ -110,9 +110,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     },
-    base: {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'OPTIONS': {'sslmode': 'require'},
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        #'OPTIONS': {'sslmode': 'require'},
         'NAME': name,
         'USER': user,
         'PASSWORD': passw,
