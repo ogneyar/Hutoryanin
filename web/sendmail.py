c8beaafe57327e9d63356cbb03a32f6b02333eb5
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-09-15 23:05:01 +0300

    Add files via upload

--- web/sendmail.py
+++ web/sendmail.py
@@ -3,9 +3,52 @@
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 
+import os, smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 
 def send(request):
 
-   return HttpResponse("<h1>Добро пожаловать!</h1>")
+    if request.get_host() == '127.0.0.1:8000':
+
+        smtp_login = "hutor_yanin@sibnet.ru"
+        smtp_pass = "***"
+        smtp_port = 25
+        smtp_server = "smtp.sibnet.ru"
+
+    else:
+
+        smtp_login = str(os.getenv("SMTP_LOGIN"))
+        smtp_pass = str(os.getenv("SMTP_PASSWORD"))
+        smtp_port = int(os.getenv("SMTP_PORT"))
+        smtp_server = str(os.getenv("SMTP_SERVER"))
+
+    '''
+    message = MIMEMultipart()
+
+    message['Subject'] = 'Test'
+    message['From'] = 'user@gmail.com'
+    message['To'] = 'someone@else.com'
+
+
+    message.attach(MIMEText('# A Heading\nSomething else in the body', 'plain'))
+    #message.attach(MIMEText('<h1 style="color: blue">A Heading</a><p>Something else in the body</p>', 'html'))
+
+    server = smtplib.SMTP(smtp_server, smtp_port)
+    server.starttls()
+    server.login(smtp_login, smtp_pass)
+    server.sendmail(smtp_login, 'ya13th@mail.ru', message.as_string())
+    server.quit()
+    '''
+
+    import email_to
+
+    server = email_to.EmailServer(smtp_server, smtp_port, smtp_login, smtp_pass)
+    server.quick_email('ya13th@mail.ru', 'Test',
+        ['# A Heading', 'Something else in the body'],
+        style='h1 {color: blue}')
+
+    return HttpResponse("<h1>Добро пожаловать!</h1>")
 
 
