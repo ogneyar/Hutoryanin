4c82b9e7e35067b05aae23e28fa4899a3083cc90
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-09-15 23:36:30 +0300

    Add files via upload

--- web/sendmail.py
+++ web/sendmail.py
@@ -7,6 +7,8 @@ import os, smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+from classes.tg.botApi import Bot
+
 
 def send(request):
 
@@ -17,12 +19,39 @@ def send(request):
         smtp_port = 25
         smtp_server = "smtp.sibnet.ru"
 
+        token = "***"
+        master = 1038937592
+
     else:
 
         smtp_login = str(os.getenv("SMTP_LOGIN"))
         smtp_pass = str(os.getenv("SMTP_PASSWORD"))
         smtp_port = int(os.getenv("SMTP_PORT"))
         smtp_server = str(os.getenv("SMTP_SERVER"))
+
+        token = os.getenv("TOKEN")
+        master = int(os.getenv("MASTER"))
+
+    response = HttpResponse()
+
+    tg = Bot(token)
+
+    if request.GET["email"] != "":
+        if request.GET["message"] != "":
+            r = tg.sendMessage(master, request.GET["email"] + "\n\n" +request.GET["message"])
+            response.write("<br><br><br><center><h1>Письмо отправленно!</h1></center><br><br>")
+            response.write("<center><a href='/'>Вернуться на главную страницу</a></center>")
+        else:
+            response.write("<br><br><br><center><h3>Необходимо описать суть вопроса!</h3></center><br><br>")
+            response.write("<center><a href='/support'>Вернуться назад</a></center>")
+    else:
+        response.write("<br><br><br><center><h3>Необходимо указать Ваш email!</h3></center><br><br>")
+        response.write("<center><a href='/support'>Вернуться назад</a></center>")
+
+
+    return response
+
+
 
     '''
     message = MIMEMultipart()
@@ -42,13 +71,16 @@ def send(request):
     server.quit()
     '''
 
+
+    '''
     import email_to
 
     server = email_to.EmailServer(smtp_server, smtp_port, smtp_login, smtp_pass)
     server.quick_email('ya13th@mail.ru', 'Test',
         ['# A Heading', 'Something else in the body'],
         style='h1 {color: blue}')
+    '''
 
-    return HttpResponse("<h1>Добро пожаловать!</h1>")
+
 
 
