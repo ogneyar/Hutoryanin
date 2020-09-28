1e3c0cbc8fc0c7e0153fa9e6c8a5f2705c1be388
ogneyar <ogneyar-ne@ya.ru>
2020-09-28 20:02:17 +0300

    Update admin.py

--- web/admin.py
+++ web/admin.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 
-from .models import Greeting, Url
+from .models import Greeting, Url, Messages
 
 
 admin.site.register(Greeting)
 admin.site.register(Url)
-#admin.site.register(Messages)
+admin.site.register(Messages)
 
