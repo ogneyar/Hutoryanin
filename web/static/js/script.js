eaef2142363d8d0a88b7994db30b406f7d83f663
ogneyar <ogneyar-ne@ya.ru>
2020-11-16 17:23:12 +0300

    qqqeeee

--- web/static/js/script.js
+++ web/static/js/script.js
@@ -3,20 +3,28 @@
     var doc = document;
 
     doc.addEventListener("DOMContentLoaded", function(){
-        console.log("Страница загрузилась!");
+        //console.log("Страница загрузилась!");
         
         var about = doc.getElementById('about');
         var text = doc.getElementById('about-text');
-        // about.onclick.style.color = "red";
+
 		about.onclick = function() {
-            //this.style.color = "red";
             if (text.style.display == "block") {
                 text.style.display = "none";
-            }else text.style.display = "block";
+                about.innerHTML = "<h4>Обо мне.</h4>";
+                about.style.color = "black";
+                about.style.border = "2px solid black";
+            }else {
+                text.style.display = "block";
+                about.innerHTML = "<h4>Скрыть текст.</h4>";
+                about.style.color = "red";
+                about.style.border = "2px solid red";
+            }
 
             //предотвращаем переход по ссылке href
             return false;
         }
+        console.log(about);
 	});	
 
 })();
