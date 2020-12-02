64fac15d24be123875cc4e7888644ec05a2a5855
ogneyar <ogneyar-ne@ya.ru>
2020-12-02 11:09:42 +0300

    b

--- web/static/js/script.js
+++ web/static/js/script.js
@@ -9,23 +9,23 @@
         var text = doc.getElementById('about-text');
 
         // функция для работы кнопки "ОБО МНЕ" на главной странице
-		about.onclick = function() {
-            if (text.style.display == "block") {
-                text.style.display = "none";
-                about.innerHTML = "<h4>Обо мне.</h4>";
-                about.style.color = "black";
-                about.style.border = "2px solid black";
-            }else {
-                text.style.display = "block";
-                about.innerHTML = "<h4>Скрыть текст.</h4>";
-                about.style.color = "red";
-                about.style.border = "2px solid red";
+        if (about) {
+            about.onclick = function() {
+                if (text.style.display == "block") {
+                    text.style.display = "none";
+                    about.innerHTML = "<h4>Обо мне.</h4>";
+                    about.style.color = "black";
+                    about.style.border = "2px solid black";
+                }else {
+                    text.style.display = "block";
+                    about.innerHTML = "<h4>Скрыть текст.</h4>";
+                    about.style.color = "red";
+                    about.style.border = "2px solid red";
+                }
+                //предотвращаем переход по ссылке href
+                return false;
             }
-
-            //предотвращаем переход по ссылке href
-            return false;
         }
-        //console.log(about);
 
     });	
 
@@ -38,7 +38,7 @@
         submit_sendmail[i].onclick = function() {
             section.style.display = "block";
         }
-        console.log('i:',i);
+        // console.log('i:',i);
     }
 
 
