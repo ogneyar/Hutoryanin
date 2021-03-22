2566da8544304e0424a11540350af7251026dc0d
ogneyar <ogneyar-ne@ya.ru>
2021-03-22 21:04:43 +0300

    test

--- hutoryanin/settings.py
+++ hutoryanin/settings.py
@@ -3,10 +3,10 @@ from pathlib import Path
 import os
 import django_heroku
 
-from dotenv import load_dotenv
-dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-if os.path.exists(dotenv_path):
-    load_dotenv(dotenv_path)
+# from dotenv import load_dotenv
+# dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+# if os.path.exists(dotenv_path):
+#     load_dotenv(dotenv_path)
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
